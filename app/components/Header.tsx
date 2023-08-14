7c97517463a32b169e5e8323ac54b06af3d68cbe
AymerickCotche <aymerick.cotche@hotmail.fr>
2023-08-14 18:42:06 +0400

    adding prisma generateb to build command

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -43,13 +43,13 @@ export default function Header({welcome}: Props) {
             <a className="mr-2">
               Accueil
             </a>
-            <span className={`absolute top-1/2 -translate-y-1/2 h-4 w-[2px] ${isScrolled ? "bg-green-700" : "bg-white"}`}></span>
+            <span className={`absolute top-1/2 -translate-y-1/2 h-4 w-[2px] ${isScrolled ? "bg-green-[#3D6367]" : "bg-white"}`}></span>
           </li>
           <li className="relative">
             <a className="mr-2">
               Portfolio
             </a>
-            <span className={`absolute top-1/2 -translate-y-1/2 h-4 w-[2px] ${isScrolled ? "bg-green-700" : "bg-white"}`}></span>
+            <span className={`absolute top-1/2 -translate-y-1/2 h-4 w-[2px] ${isScrolled ? "bg-green-[#3D6367]" : "bg-white"}`}></span>
           </li>
           <li className="relative">
             <a>
