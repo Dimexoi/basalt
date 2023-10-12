cdd8ec96201ee5dcb620515d47e09fb6d6f7c26d
AymerickCotche <aymerick.cotche@hotmail.fr>
2023-10-12 23:27:39 +0400

    Enabling Vercel Analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 import { Providers } from "@/redux/provider"
 import Header from './components/Header'
@@ -30,6 +31,7 @@ export default function RootLayout({
           <Footer/>
         </SessionProviders>
       </Providers>
+      <Analytics />
       </body>
   </html>
   )
