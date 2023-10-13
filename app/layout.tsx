d758842f8c5182fcf66cc73a30be370b0a13e9c7
AymerickCotche <aymerick.cotche@hotmail.fr>
2023-10-13 12:30:15 +0400

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,12 +7,14 @@ import { Providers } from "@/redux/provider"
 import Header from './components/Header'
 import Footer from './components/Footer'
 import SessionProviders from './components/SessionsProviders'
+import Favicon from '@/public/images/logo-icon.png';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Basalt Mobilier Pro',
   description: 'Basalt mobilier pro homepage',
+  icons: [{ rel: 'icon', url: Favicon.src }],
 }
 
 export default function RootLayout({
