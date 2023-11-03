546016179e62a45f8bc1861d4530899656bf6b36
AymerickCotche <aymerick.cotche@hotmail.fr>
2023-11-03 10:31:19 +0400

    add facebook pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
 import FacebookPixel from './components/FacebookPixel'
-import Script from 'next/script'
+
 import { Providers } from "@/redux/provider"
 import Footer from './components/Footer'
 import SessionProviders from './components/SessionsProviders'
@@ -41,25 +41,8 @@ export default function RootLayout({
       </Providers>
 
       <Analytics />
-
-      <Script
-        id="fb-pixel"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-        __html: `
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', ${process.env.FACEBOOK_PIXEL_ID});
-        fbq('track', 'PageView');
-        `,
-        }}
-      />
+      
+      <FacebookPixel />
     </body>
   </html>
   )
