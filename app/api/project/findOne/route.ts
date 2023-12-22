99e783e83fb40c18c1e468a70f9bbb4c3a2be860
AymerickCotche <aymerick.cotche@hotmail.fr>
2023-12-22 13:33:58 +0400

    edit sitemap

--- app/api/project/findOne/route.ts
+++ app/api/project/findOne/route.ts
@@ -5,8 +5,7 @@ import prisma from "@/lib/prisma"
 
 export async function POST(req: Request) {
   try {
-    const body = await req.json()
-    const {id} = body
+    const id = await req.json()
     const results = await prisma.project.findUnique({
       where: {
         id: Number(id)
