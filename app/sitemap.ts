708107ffd1a880112236165b42d554d543337d01
AymerickCotche <aymerick.cotche@hotmail.fr>
2023-12-22 11:41:42 +0400

    edit sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,57 +4,39 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://basalt-mobilier.fr',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
+      lastModified: new Date()
     },
     {
       url: 'https://basalt-mobilier.fr/mentions-legales',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.7,
+      lastModified: new Date()
     },
     {
       url: 'https://basalt-mobilier.fr/politique-de-confidentialite',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.7,
     },
     {
       url: 'https://basalt-mobilier.fr/plan-du-site',
       lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
     },
     {
       url: 'https://basalt-mobilier.fr/category/2/hotels',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
+      lastModified: new Date()
     },
     {
       url: 'https://basalt-mobilier.fr/category/1/restaurants',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
+      lastModified: new Date()
     },
     {
       url: 'https://basalt-mobilier.fr/category/3/locations-saisonnieres',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
+      lastModified: new Date()
     },
     {
       url: 'https://basalt-mobilier.fr/category/5/moodboards',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
+      lastModified: new Date()
     },
     {
       url: 'https://basalt-mobilier.fr/category/6/tout-voir',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
+      lastModified: new Date()
     },
   ]
 }
