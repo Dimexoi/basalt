b54ab0068f61af469382287a46755235072d95a3
AymerickCotche <aymerick.cotche@hotmail.fr>
2024-02-17 16:05:27 +0400

    edit

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -8,6 +8,7 @@ interface RequestBody {
 
 export async function POST(request:Request) {
   const body: RequestBody = await request.json()
+  console.error(body);
 
   const user = await prisma.user.create({
     data: {
